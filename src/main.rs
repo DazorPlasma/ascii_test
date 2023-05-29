59eea90a26c146248c52b9cd6797e737843158e1
DazorPlasma <dazorplasma@proton.me>
2023-05-29 10:28:11 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ascii::AsciiStr;
 fn main() {
-    println!("Hello, world!");
+    let s = AsciiStr::from_ascii("asdfghjkl").unwrap();
+    for chr in s.as_bytes() {
+        print!("{} ", *chr as char);
+    }
+    println!("");
+    for chr in s.chars() {
+        print!("{} ", chr);
+    }
+    println!("");
 }
